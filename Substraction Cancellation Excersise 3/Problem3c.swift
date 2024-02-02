c0db600c1e94a6c08595d71a8356ba2ff8222bbd
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-02-02 13:40:52 -0600

    Changes were made.

--- Substraction Cancellation Excersise 3/Problem3c.swift
+++ Substraction Cancellation Excersise 3/Problem3c.swift
@@ -1,1 +1,10 @@
+//
+//  ContentView.swift
+//  HW 2
+//
+//  Problem 3c - Obsering the linear regime on your graph and explain why the downward sum is generally more precise.
+//  Created by PHYS 440 Marco on 1/24/24.
+//
+
+
 
